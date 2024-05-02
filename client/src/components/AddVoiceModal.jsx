ed1cc8c38e113a500a8814411effb7db261036a1
BakerRang <bakerz2009@gmail.com>
2024-05-02 09:11:07 -0600

    Adding support to do voice recording directly on the app

--- client/src/components/AddVoiceModal.jsx
+++ client/src/components/AddVoiceModal.jsx
@@ -1,14 +1,32 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { DropzoneWrapper, InputWrapper, LoadingSpinner } from './index.js'
 import { request } from '../utils/index.js'
 import { SERVER_PREFIX } from '../App.jsx'
 import { Alert } from '@material-tailwind/react'
+import AudioRecorder from './AudioRecorder.jsx'
 
 const AddVoiceModal = ({ open, success, cancel }) => {
   const [files, setFiles] = useState([])
   const [name, setName] = useState('')
   const [description, setDescription] = useState('')
   const [isSaving, setIsSaving] = useState(false)
+  const [audioUrl, setAudioUrl] = useState(null)
+  const [audioBlob, setAudioBlob] = useState(null)
+  const [recording, setRecording] = useState(null)
+
+  useEffect(() => {
+    if (audioBlob) {
+      const audioFile = new File([audioBlob], 'recording.mp3', { type: 'audio/mp3' })
+      setRecording(audioFile)
+      setFiles([])
+    }
+  }, [audioBlob])
+
+  useEffect(() => {
+    if (files.length > 0) {
+      clearRecording()
+    }
+  }, [files])
 
   const handleSave = async () => {
     setIsSaving(true)
@@ -20,10 +38,20 @@ const AddVoiceModal = ({ open, success, cancel }) => {
       formData.append('files', file, file.name)
     })
 
+    if (recording) {
+      formData.append('files', recording, recording.name)
+    }
+
     const saveRes = await request(`${SERVER_PREFIX}/text/to/speech/v1/voice`, 'POST', null, formData)
     const voice = await saveRes.json()
     setIsSaving(false)
     success(voice)
+  }
+
+  const clearRecording = () => {
+    setAudioUrl(null)
+    setAudioBlob(null)
+    setRecording(null)
   }
 
   return (
@@ -56,6 +84,20 @@ const AddVoiceModal = ({ open, success, cancel }) => {
             <InputWrapper value={name} setValue={setName} label='Name' className='mb-2' />
             <InputWrapper value={description} setValue={setDescription} label='Description' className='mb-2' />
             <DropzoneWrapper files={files} setFiles={setFiles} />
+            <span className='text-white text-sm flex justify-center mt-5'>Or record here</span>
+            <AudioRecorder className='flex justify-center mt-3' setAudioURL={setAudioUrl} setAudioBlob={setAudioBlob} />
+            {audioUrl && (
+              <div className='flex justify-center items-center w-full h-6 mt-4'>
+                <audio controls src={audioUrl} className='h-6'>
+                  Your browser does not support the audio element.
+                </audio>
+                <button onClick={clearRecording} className='ml-4 text-red-500 hover:text-red-400'>
+                  <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-6 h-6'>
+                    <path strokeLinecap='round' strokeLinejoin='round' d='M6 6l12 12m0-12L6 18' />
+                  </svg>
+                </button>
+              </div>
+            )}
             <div className='flex justify-end mt-4'>
               <button className='mr-2 px-4 py-2 text-white bg-gray-500 hover:bg-gray-700 rounded' onClick={cancel}>
                 Cancel
@@ -63,7 +105,7 @@ const AddVoiceModal = ({ open, success, cancel }) => {
               <button
                 className='px-4 py-2 text-white bg-blue-500 hover:bg-blue-700 rounded disabled:opacity-25'
                 onClick={handleSave}
-                disabled={!files.length || !name.length || isSaving}
+                disabled={(!files.length && !recording) || !name.length || isSaving}
               >
                 {isSaving ? <LoadingSpinner svgClassName='!h-4 !w-4' /> : 'Save'}
               </button>
--- client/src/components/AudioRecorder.jsx
+++ client/src/components/AudioRecorder.jsx
@@ -26,7 +26,7 @@ const AudioRecorder = ({ className, setAudioBlob, setAudioURL, setAudioBase64 })
         }
 
         recorder.onstop = async e => {
-          const audioBlob = new Blob(audioChunks, { type: 'audio/wav' })
+          const audioBlob = new Blob(audioChunks, { type: 'audio/mp3' })
           const audioUrl = URL.createObjectURL(audioBlob)
           const base64 = await blobToBase64(audioBlob)
           if (setAudioURL) {
@@ -60,15 +60,15 @@ const AudioRecorder = ({ className, setAudioBlob, setAudioURL, setAudioBase64 })
     <div className={className}>
       {!isRecording
         ? (
-          <button onClick={startRecording} className='pt-4 bg-gray-700 rounded-full hover:text-blue-500'>
-            <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth='1.5' stroke='currentColor' className='bg-gray-700 w-6 h-6'>
+          <button onClick={startRecording} className='bg-gray-700 rounded-full text-white hover:text-blue-500 w-10 h-10 flex items-center justify-center p-0'>
+            <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth='1.5' stroke='currentColor' className='w-3/5 h-3/5 self-center'>
               <path strokeLinecap='round' strokeLinejoin='round' d='M12 18.75a6 6 0 006-6v-1.5m-6 7.5a6 6 0 01-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 01-3-3V4.5a3 3 0 116 0v8.25a3 3 0 01-3 3z' />
             </svg>
           </button>
           )
         : (
-          <button onClick={stopRecording} className='pt-4 rounded-full bg-gray-700'>
-            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor' className='w-6 h-6 text-red-500 animate-pulse'>
+          <button onClick={stopRecording} className='bg-gray-700 rounded-full w-10 h-10 flex items-center justify-center p-0'>
+            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor' className='w-3/5 h-3/5 self-center text-red-500 animate-pulse'>
               <path d='M8.25 4.5a3.75 3.75 0 117.5 0v8.25a3.75 3.75 0 11-7.5 0V4.5z' />
               <path d='M6 10.5a.75.75 0 01.75.75v1.5a5.25 5.25 0 1010.5 0v-1.5a.75.75 0 011.5 0v1.5a6.751 6.751 0 01-6 6.709v2.291h3a.75.75 0 010 1.5h-7.5a.75.75 0 010-1.5h3v-2.291a6.751 6.751 0 01-6-6.709v-1.5A.75.75 0 016 10.5z' />
             </svg>
